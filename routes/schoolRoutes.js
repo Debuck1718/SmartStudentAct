4115c5168e35435cb8573cca1a2288fd5c702753
Debuck1718 <evans.buckman55@gmail.com>
2025-08-19 20:52:43 -0700

    Delete routes directory

--- routes/schoolRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/schoolRoutes.js
-
-const express = require('express');
-const router = express.Router();
-const { addSchool } = require('../controllers/schoolController');
-
-router.post('/', addSchool);
-
-module.exports = router;
