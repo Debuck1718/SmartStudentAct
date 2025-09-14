7cdc8125977be1b0eebb80af51994342521b8835
Debuck1718 <evans.buckman55@gmail.con>
2025-09-13 22:12:50 -0700

    update server

--- Backend/server.js
+++ Backend/server.js
@@ -53,14 +53,9 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
+    origin: "*", // allow all origins
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"], // allow all HTTP methods
     allowedHeaders: [
       "Content-Type",
       "Authorization",
